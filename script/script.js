4b8825a5bc0daad4efb927918f245c892c81b53d
rprethi <2271821@cmontmorency.qc.ca>
2024-03-27 22:58:10 -0400

    init

---
+++ script/script.js
@@ -0,0 +1,21 @@
+// Scrolling Body
+let chapScroll;
+const body = document.querySelector("body");
+
+window.addEventListener("scroll", function () {
+  body.classList.add("is-scrolling");
+  window.clearTimeout(chapScroll);
+
+  chapScroll = setTimeout(function () {
+    body.classList.remove("is-scrolling");
+  }, 100);
+});
+
+// Animation Appel à l'action (Introduction)
+const play = document.querySelector(".action");
+
+gsap.from(".action", {
+  repeat: -1,
+  y: "-20",
+  yoyo: true,
+});
